997158f03ba9c6b8f31019583e547682d8eefdee
Icaro Silva <7913016+ikaromarlon@users.noreply.github.com>
2025-01-07 19:08:22 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ To install dofiles run:
 ```bash
 ./install.sh
 ```
+
+*Note: If you face any error due to missing zsh, nvm and etc, fix them and run the installation again.*
--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 
 # get current directory
-DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+DIR=$(dirname $(realpath "$0"))
 
-echo "Script Directory: $DIR"
+# get zsh-autosuggestions directory
+ZSH_AUTOSUGGESTIONS_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-ln -sf $DIR/home/.gitconfig ~/.gitconfig
-ln -sf $DIR/home/.profile ~/.profile
-ln -sf $DIR/home/.env ~/.env
-ln -sf $DIR/home/.shell_aliases ~/.shell_aliases
-ln -sf $DIR/home/.zshrc_hooks ~/.zshrc_hooks
-ln -sf $DIR/home/.zshrc ~/.zshrc
+echo "Installing oh-my-zsh..."
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf $ZSH_AUTOSUGGESTIONS_DIR
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOSUGGESTIONS_DIR
+
+echo "Setting up dotfiles..."
+
+ln -sf "$DIR/home/.gitconfig" ~/.gitconfig
+ln -sf "$DIR/home/.profile" ~/.profile
+ln -sf "$DIR/home/.env" ~/.env
+ln -sf "$DIR/home/.shell_aliases" ~/.shell_aliases
+ln -sf "$DIR/home/.zshrc_hooks" ~/.zshrc_hooks
+ln -sf "$DIR/home/.zshrc" ~/.zshrc
+
+source ~/.zshrc
+
+echo "Dotfiles set up successfully!"
